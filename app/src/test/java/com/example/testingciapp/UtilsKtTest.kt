3bfebc6ec8617bf93d9f7cd44684e58525cce56e
Ramzy Kermad <kermad.r@sncf.fr>
2023-12-18 18:20:35 +0100

    Added som test to check RG for Utils check password function

--- app/src/test/java/com/example/testingciapp/UtilsKtTest.kt
+++ app/src/test/java/com/example/testingciapp/UtilsKtTest.kt
@@ -7,6 +7,29 @@ import org.junit.Test
 class UtilsKtTest {
 
     @Test
-    fun verifyPassword() {
+    fun test_ValidPassword() {
+        // Setup
+        val password = "Passw0rd!"
+
+        // Action
+        val results = verifyPassword(password)
+
+        // Result
+        assertTrue(results.isEmpty())
     }
+
+    @Test
+    fun `Password less than 6 alphanumerical characters`() {
+        // Setup
+        val password = "Pwd1!"
+
+        // Action
+        val results = verifyPassword(password)
+
+        // Result
+        assertEquals(results.size, 1)
+        assertEquals("Le mot de passe doit contenir au moins 6 caractères.", results[0])
+
+    }
+
 }
